5a331ecd983ef346c770598009fb69c4208357cb
KangByungWook <brillante91@naver.com>
2015-09-03 16:21:33 +0000

    lab_db_complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -405,6 +405,142 @@ a.review_point=1.2
 a.region_code="01"
 a.save
 
+#경기
+a=TestCenter.new
+a.name="한국항공대학교 "
+a.address="경기 고양시 덕양구 화전동 200-1"
+a.position_lat="37.600619"
+a.position_lng="126.864399"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="안양대학교 "
+a.address="경기도 안양시 만안구 안양5동 708-113번지"
+a.position_lat="37.391799"
+a.position_lng="126.919775"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="성결대학교"
+a.address="경기 안양시 만안구 안양8동 400-10"
+a.position_lat="37.380023"
+a.position_lng="126.928622"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="오산대학교"
+a.address="경기도 오산시 청학동 17번지"
+a.position_lat="37.155762"
+a.position_lng="127.061212"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="서울신학대학교"
+a.address="경기도 부천시 소사구 소사본2동"
+a.position_lat="37.479446"
+a.position_lng="126.789018"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="수원여자대학교"
+a.address="경기 수원시 권선구 오목천동 산 1-6번지"
+a.position_lat="37.252272"
+a.position_lng="126.963576"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="수원대학교"
+a.address="경기도 화성시 봉담읍 와우리 산 2-2"
+a.position_lat="37.209240"
+a.position_lng="126.976998"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="가천대학교 글로벌캠퍼스"
+a.address="경기도 성남시 수정구"
+a.position_lat="37.451225"
+a.position_lng="127.129278"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="성남상공회의소"
+a.address="경기도 성남시 분당 이매동(이매촌) 113번지"
+a.position_lat="37.398325"
+a.position_lng="127.125804"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="대진대학교"
+a.address="경기도 포천시 선단동"
+a.position_lat="37.873109"
+a.position_lng="127.156825"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="청강문화산업대학"
+a.address="경기도 이천시 마장면 청강로 162"
+a.position_lat="37.204148"
+a.position_lng="127.354682"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="신흥대학교"
+a.address="경기도 의정부시 호원1동 117번지"
+a.position_lat="37.709583"
+a.position_lng="127.045482"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="한세대학교"
+a.address="경기도 군포시 당정동 604-5번지"
+a.position_lat="37.345533"
+a.position_lng="126.953323"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="국제통상고등학교"
+a.address="경기도 부천시 원미구 중1동 1171-1번지"
+a.position_lat="37.500395"
+a.position_lng="126.761324"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
+a=TestCenter.new
+a.name="한국외대 용인캠퍼스"
+a.address="경기도 용인시 처인구 모현면 왕산리 산 89"
+a.position_lat="37.337518"
+a.position_lng="127.268493"
+a.review_point=1.2
+a.region_code="01"
+a.save
+
 
 
 
@@ -497,7 +633,364 @@ a=TestCenter.new
 a.name="대덕대학교"
 a.address="대전광역시 유성구 장동 대덕대학"
 a.position_lat="36.390691"
-a.position_lng="36.390691"
+a.position_lng="127.365443"
 a.review_point=1.2
 a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="배재대학교"
+a.address="대전광역시 서구 연자 1길 14(도마동)"
+a.position_lat="36.320636"
+a.position_lng="127.366165"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="한국정보대학교"
+a.address="대전광역시 유성구 문지로 119번지(문지동 103-6)"
+a.position_lat="36.392191"
+a.position_lng="127.398784"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="나사렛대학교"
+a.address="충청남도 천안시 쌍용동 456번지"
+a.position_lat="36.795127"
+a.position_lng="127.118984"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="상명대학교 천안캠퍼스"
+a.address="충남 천안시 안서동 98-20 상명대학교"
+a.position_lat="36.832916"
+a.position_lng="127.178077"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="순천향대학교"
+a.address="충남 아산시 신창면 읍내리 646"
+a.position_lat="36.769700"
+a.position_lng="126.931690"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="선문대학교"
+a.address="충청남도 아산시 탕정면 갈산리 100"
+a.position_lat="36.769640"
+a.position_lng="126.931701"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="청주교육대학교"
+a.address="충청북도 청주시 흥덕구 청남로 330"
+a.position_lat="36.616127"
+a.position_lng="127.484603"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="건국대학교 충주캠퍼스"
+a.address="충북 충주시 단월동 322번지"
+a.position_lat="36.949508"
+a.position_lng="127.907401"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="공주대학교"
+a.address="충청남도 공주시 신관동 182번지"
+a.position_lat="36.469595"
+a.position_lng="127.141025"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="홍익대학교 세종캠퍼스"
+a.address="충남 연기군 조치원읍 신안리 300"
+a.position_lat="36.620845"
+a.position_lng="127.287128"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="건양대학교"
+a.address="충청남도 논산시 내동 26번지"
+a.position_lat="36.182645"
+a.position_lng="127.111310"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="카이스트 "
+a.address="충남 연기군 조치원읍 신안리 300"
+a.position_lat="36.369388"
+a.position_lng="127.364046"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="충북보건과학대학교"
+a.address="충북 청원군 내수읍 덕암리 산 4번지"
+a.position_lat="36.702155"
+a.position_lng="127.551850"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+a=TestCenter.new
+a.name="충북대학교"
+a.address="충북 청주시 흥덕구 성봉로 410"
+a.position_lat="36.628646"
+a.position_lng="127.457572"
+a.review_point=1.2
+a.region_code="03"
+a.save
+
+
+
+#경상도(04)
+a=TestCenter.new
+a.name="경성대학교"
+a.address="부산 남구 대연동 산110-1"
+a.position_lat="35.139778"
+a.position_lng="129.098459"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="부산대학교"
+a.address="부산 금정구 장전동 산 30"
+a.position_lat="35.233921"
+a.position_lng="129.079308"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="신라대학교"
+a.address="부산 사상구 괘법동 산 1-1"
+a.position_lat="35.168187"
+a.position_lng="128.995871"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="부산교육대학교"
+a.address="부산 연제구 거제1동 263"
+a.position_lat="35.195421"
+a.position_lng="129.075074"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="고신대학교"
+a.address="부산 영도구 동삼동 149-1"
+a.position_lat="35.079626"
+a.position_lng="129.063007"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="한국해양대학교"
+a.address="부산 영도구 동삼동 1"
+a.position_lat="35.074694"
+a.position_lng="129.088233"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="영산대학교"
+a.address="경상남도 양산시 주남동 산 150"
+a.position_lat="35.428784"
+a.position_lng="129.145971"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="부경대학교"
+a.address="부산시 남구 대연3동 599-1"
+a.position_lat="35.133832"
+a.position_lng="129.103670"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="울산상공회의소"
+a.address="울산 남구 신정3동 589-1"
+a.position_lat="35.539556"
+a.position_lng="129.316442"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="선린대학교"
+a.address="경북 포항시 북구 흥해읍 초곡동 146-1"
+a.position_lat="36.091473"
+a.position_lng="129.333136"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="영남대학교 경산캠퍼스"
+a.address="경북 경산시 대동 214-1"
+a.position_lat="35.830183"
+a.position_lng="128.757355"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="대구카톨릭대학교"
+a.address="경상북도 경상시 하양읍 금락로5(금라)"
+a.position_lat="35.911157"
+a.position_lng="128.807678"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="영진전문대학교"
+a.address="대구북구 복현동 들샘4길 41"
+a.position_lat="35.896305"
+a.position_lng="128.622040"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="계명대학교 대명캠퍼스"
+a.address="대구 남구 대명동 2139"
+a.position_lat="35.857396"
+a.position_lng="128.485539"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="경북대학교"
+a.address="대구 북구 산격동 1370, 대구 중구 동인동 101"
+a.position_lat="35.888805"
+a.position_lng="128.610324"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="경주대학교"
+a.address="경북 경주시 효현동 산42-1"
+a.position_lat="35.829583"
+a.position_lng="129.162555"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="위덕대학교"
+a.address="경북 경주시 강동면 유금리 525"
+a.position_lat="36.012743"
+a.position_lng="129.283083"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="경상대학교"
+a.address="경상남도 진주시 진주대로 501"
+a.position_lat="35.154027"
+a.position_lng="128.098084"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+a=TestCenter.new
+a.name="창원대학교"
+a.address="경상남도 창원시 의창구 창원대학로 20"
+a.position_lat="35.245597"
+a.position_lng="128.691884"
+a.review_point=1.2
+a.region_code="04"
+a.save
+
+#전라도(05)
+a=TestCenter.new
+a.name="전북대학교"
+a.address="전북 전주시 덕진구 덕진동 1가 664-14"
+a.position_lat="35.846990"
+a.position_lng="127.129363"
+a.review_point=1.2
+a.region_code="05"
+a.save
+
+a=TestCenter.new
+a.name="전남대학교"
+a.address="광주 북구 용봉동 300"
+a.position_lat="35.176142"
+a.position_lng="126.905818"
+a.review_point=1.2
+a.region_code="05"
+a.save
+
+a=TestCenter.new
+a.name="목포대학교"
+a.address="전남 무안군 청계면 도림리 61"
+a.position_lat="34.910406"
+a.position_lng="126.436059"
+a.review_point=1.2
+a.region_code="05"
+a.save
+
+#강원도(06)
+a=TestCenter.new
+a.name="경동대학교"
+a.address="강원 고성군 토성면 봉포리 산 91-1"
+a.position_lat="38.250613"
+a.position_lng="128.555511"
+a.review_point=1.2
+a.region_code="06"
+a.save
+
+a=TestCenter.new
+a.name="강원대학교"
+a.address="강원도 춘천시 효자2동 192-1번지"
+a.position_lat="37.870048"
+a.position_lng="127.743365"
+a.review_point=1.2
+a.region_code="06"
+a.save
+
+#제주도(07)
+a=TestCenter.new
+a.name="제주대학교"
+a.address="제주특별자치도 제주시 제주대학로 66번지"
+a.position_lat="33.455263"
+a.position_lng="126.561607"
+a.review_point=1.2
+a.region_code="07"
 a.save
