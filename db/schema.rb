6205ad4a43ba5612bacbe2dab06b4f1bf169498c
draconian00 <korea_syh@naver.com>
2015-09-08 08:44:19 +0000

    info_fix

--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20150908071034) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.integer  "plus_count"
+    t.integer  "minus_count"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "image"
     t.string   "nickname"
